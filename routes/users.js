d4ffe1718c683513863bdb226b9ac8a5d29561d1
Bryce <antiteal@gmail.com>
2018-11-18 17:45:01 -0800

    login wip

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let router = express.Router();
-let models = require('../models');
+let userModel = require('../models/users.js');
 let bcrypt = require('bcryptjs');
 let db = require('../models/index.js');
 
@@ -15,43 +15,102 @@ router.get('/register', function(req, res, next) {
 
 router.post('/register', function(req, res, next) {
   let email = req.body.email;
-  let githubHandle = req.body.github_handle;
   let password = req.body.password;
   let confirmPassword = req.body.confirm_password;
 
   req.checkBody('email', 'Email is required').notEmpty();
   req.checkBody('email', 'Email is not valid').isEmail();
-  req.checkBody('github_handle', 'GitHub Handle is required').notEmpty();
   req.checkBody('password', 'Password is required').notEmpty();
   req.checkBody('confirm_password', 'Passwords do not match').equals(req.body.password);
+  req.checkBody('password', 'Password must be at least 7 characters').isLength({
+    min: 7
+  });
+  req.checkBody('password', 'Password must contain at least 1 number').matches(/\d/);
+  req.checkBody('password', 'Password must contain characters').matches(/[a-zA-z]/);
 
   let errors = req.validationErrors();
 
-  if (errors) {
-    res.render('user-register', {
-      errors: errors,
-    });
-  } else {
-    bcrypt.genSalt(10, function(err, salt) {
-      bcrypt.hash(password, salt, function(err, hash) {
-        if (err) {
-          throw err;
-        }
-
-        models.Users.create({
-          email: email,
-          github_handle: githubHandle,
-          password: hash,
-        })
-        .then((newUser) => {
-          console.log(newUser.toJSON());
-        });
-
+  db['users'].findOne({
+    where: {
+      email: email
+    }
+  }).then(user => {
+    if (user != null) {
+      if (!errors) 
+        errors = [];
+      
+      errors.push({
+        param: 'email',
+        msg: 'Email is already registered',
+        value: email
       });
-    });
-    req.flash('success_msg', 'You are registered and can now login');
-    res.redirect('/users/login');
-  }
+    }
+
+    if (errors) {
+      res.render('user-register', {
+        errors: errors,
+      });
+    } 
+    else {
+      bcrypt.hash(password, 10, function(err, hash) {
+        db['users'].create({
+          email: email,
+          password: hash,
+        });
+      });
+      req.flash('success_msg', 'You are registered and can now login');
+      res.redirect('/users/login');
+    }
+  });
+});
+
+router.post('/login', function(req, res, next) {
+  let email = req.body.email;
+  let password = req.body.password;
+
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('password', 'Password is required').notEmpty();
+
+  let errors = req.validationErrors();
+
+  db['users'].findOne({
+    where: {
+      email: email
+    }
+  }).then(user => {
+    if (user == null) {
+      if (!errors) 
+        errors = [];
+      
+      errors.push({
+        param: 'email',
+        msg: 'No user with that email exists',
+        value: email
+      });
+    }
+
+    if(errors) {
+      res.render('user-login', {
+          errors: errors,
+      });
+    }
+    else {
+      bcrypt.compare(password, user.password, function(err, correct) {
+        if(err) {
+          req.flash('error_msg', err);
+          res.redirect('/users/login');
+        }
+        else if(correct) {
+          req.flash('success_msg', 'You are now logged in');
+          res.redirect('/');
+        }
+        else {
+          req.flash('error_msg', 'Incorrect password');
+          res.redirect('/users/login');
+        }
+      });
+    }
+  });
 });
 
 router.get('/login', function(req, res, next) {
